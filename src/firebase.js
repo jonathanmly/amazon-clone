28ccd475b373df18c793ba5752021d510009b983
jonathan ly <jonathan.m.ly@gmail.com>
2021-02-05 10:00:46 -0800

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+<script>
+  var firebaseConfig = {
+    apiKey: "AIzaSyA67EY4DkA-6q0T7mLJ2C2tgcoYTmuR3K0",
+    authDomain: "clone-bf97e.firebaseapp.com",
+    projectId: "clone-bf97e",
+    storageBucket: "clone-bf97e.appspot.com",
+    messagingSenderId: "381217754773",
+    appId: "1:381217754773:web:0aa78b64a670b88addd59c",
+    measurementId: "G-ZMSZRKJ2PS"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
